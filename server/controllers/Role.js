77d3fe6c87096a64cd913a77184f9f73d099df0a
Anthony Ngene <anthony.ngene@andela.com>
2017-04-30 11:23:00 +0100

    undo previous migration before performing a new migration
    [#144518023]

--- server/controllers/Role.js
+++ server/controllers/Role.js
@@ -1,12 +1,24 @@
 import models from '../models';
 
-export default {
+const Role = {
+  /**
+  * Create a new role
+  * Route: GET: /roles
+  * @param {Object} req request object
+  * @param {Object} res response object
+  * @returns {void} no returns
+  */
   create(req, res) {
-    console.log(req.body.name);
-    return models.Role.create({
-      name: 'admin'
-    })
-    .then(todo => res.status(201).send(todo))
+    return models.Role.create(req.body)
+    .then(role => res.status(201).send({
+      id: role.id,
+      name: role.name,
+      message: 'Role created'
+    }))
     .catch(error => res.status(400).send(error));
   }
+
+  // End
 };
+
+export default Role;
