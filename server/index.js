5209c0b3f9df0445eeb26387e754bfd9a9873da3
Anthony Ngene <anthony.ngene@andela.com>
2017-05-14 11:35:37 +0100

    chore(frontend): configure webpack and make the index route available to the client
    [#145029929]

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
-/* eslint no-console: "off" */
+import path from 'path';
 import morgan from 'morgan';
 import app from './app';
 
 let port = process.env.PORT || 3000;
+let homepage = path.join(__dirname, '../client/index.html');
+
 if (process.env.NODE_ENV === 'development') {
+  const config = require('../../webpack.config.dev');
+  const compiler = require('webpack')(config);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000
+  }));
   app.use(morgan('dev'));
   port = 8080;
+  homepage = path.join(__dirname, '../../client/index.html');
+} else {
+  app.use(require('compression')());
+  app.use(require('express').static('lib/client'));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(homepage);
+});
 
 app.listen(port, () => {
   console.log('Magic happening at: ', port);
---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production')
+};
+
+module.exports = {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: path.resolve(__dirname, 'client/index'),
+  target: 'web',
+  output: {
+    path: __dirname + '/lib/client',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './lib/client')
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin(GLOBALS),
+    new ExtractTextPlugin('style.css'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'client'), loaders: ['babel']},
+      {test: /(\.css)$/, loader: ExtractTextPlugin.extract('css?sourceMap')},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
